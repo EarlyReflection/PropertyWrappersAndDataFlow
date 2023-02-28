d3a9e21ee25ed8be91f1065a7860bdcaa9510f16
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-02-28 15:35:13 +0200

    Initial Commit

---
+++ PropertyWrappersAndDataFlow/PropertyWrappersAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersAndDataFlowApp.swift
+//  PropertyWrappersAndDataFlow
+//
+//  Created by Vladimir Dvornikov on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
